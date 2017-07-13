8bb60637ed333529a7b82a24df40dbf2efa32238
Sergey <sergeypanichev@live.ru>
2017-07-13 18:42:25 +0700

    final

--- main.py
+++ main.py
@@ -11,10 +11,6 @@ from matplotlib import pyplot as plt
 
 
 def f(x):
-    # return 10 * np.e ** (0.1 * x) * np.sin(0.6 * np.pi * x) # Функция для проверки rbf
-    # return x**3 - 20*x**2 + 100*x - 40 # Полиномиальная функция
-    # return np.sinc(0.3*x) # Категоричесий синус
-    # return np.sin(x) / x # Стандартная функция для тестирования
     if x > 3.5 and x < 5.5:
         return np.e**(-(x - 4.7)**2)/0.3
     if x > 9.8 and x < 13.2:
@@ -191,32 +187,12 @@ def main():
                 thread.join()
 
     # Исследования
-    datafile = "Datasets\\wtf\\wtf_0.1_30.xlsx"
+    datafile = "Datasets\\test\\test_0.1_30.xlsx"
     data = pd.read_excel(datafile, header=0)
     X_data = data.drop("y", axis=1).as_matrix()
-    X_test = pd.read_excel("Datasets\\wtf\\test.xlsx", header=0).as_matrix()
+    X_test = pd.read_excel(datafile, header=0).as_matrix()
     Y_data = np.array(data["y"])
     researches("LSSVR", mk_mode=True)
-
-    # plt.figure(dpi=200, figsize=(10, 6))
-    # plt.grid(True)
-    # plt.xlabel("X (attrubute)", fontsize=14)
-    # plt.ylabel("Y (response)", fontsize=14)
-    # x_cur = [X_data[i][0] for i in range(len(X_data))]
-    # x_est = [X_test[i][0] for i in range(len(X_test))]
-    # y_cur = []
-    # for i in range(len(x_est)):
-    #     y_cur.append(f(x_est[i]))
-    # plt.plot(x_cur, Y_data, 'bo', label="Data", alpha=0.5)
-    # plt.plot(x_est, y_cur, 'r--', label="Real function", alpha=0.7)
-    # # plt.plot(x_est, y_est, "k-", label="Estimated function")
-    # plt.legend(prop={'size': 14})
-    # plt.savefig("dataset.jpeg")
-
-    # Генерация наборов данных
-    # for i in [0.1]:
-    #     for j in [0.5]:
-    #         data_generate("Datasets\\wtf\\", "wtf", np.arange(-15.0, 15.0, j), i)
 
 
 if __name__ == '__main__':
